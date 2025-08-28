3eb16aaf32daa4e43d8b6ec59dfde60ba8d82e46
bozhidarzlatev <156793066+bozhidarzlatev@users.noreply.github.com>
2025-08-28 13:53:12 +0300

    Feed endpoint added

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -136,10 +136,31 @@ const replyPost = async (req, res) => {
 
 }
 
+const getFeedPost = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: `User not found!` })
+        }
+
+        const following = user.following;
+
+        const feedPosts = await Post.find({postedBy: {$in:following}}).sort({createdAt: -1});
+
+        res.status(200).json(feedPosts)
+
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+        console.log(`Error in replyPost : `, error.message);
+    }
+}
+
 export const postController = {
     createPost,
     getPost,
     deletePost,
     likeUnlikePost,
-    replyPost
+    replyPost,
+    getFeedPost
 }
