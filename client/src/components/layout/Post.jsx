896963d839816426489a2c52327c005842dfde55
bozhidarzlatev <156793066+bozhidarzlatev@users.noreply.github.com>
2025-09-04 17:27:18 +0300

    date-fns library added

--- client/src/components/layout/Post.jsx
+++ client/src/components/layout/Post.jsx
@@ -1,27 +1,28 @@
 import { Flex, Avatar, AvatarGroup, Box, Text, Image } from "@chakra-ui/react";
-import { BsThreeDots } from "react-icons/bs";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Actions from "./Actions";
 import { useEffect, useState } from "react";
 import useShowToast from "../../hooks/useShowToast";
+import { formatDistance } from "date-fns"
 
 export default function Post({ post, postedBy }) {
     const [liked, setLiked] = useState(false);
     const [user, setUser] = useState(null)
     const showToast = useShowToast();
+    const navigate = useNavigate()
 
     useEffect(() => {
         const getUser = async () => {
             try {
                 const res = await fetch("/api/users/profile/" + postedBy);
                 const data = await res.json()
-                if(data.error) {
+                if (data.error) {
                     showToast(false, data.error);
                     return
-                } else{
+                } else {
                     setUser(data)
                 }
-                
+
             } catch (error) {
                 setUser(null)
                 showToast(false, error);
@@ -32,49 +33,63 @@ export default function Post({ post, postedBy }) {
 
         getUser()
     }, [postedBy])
-    
+
     return (
-        
-        <Link to="/markzuckerberg/post/1">
+
+        <Link to={`/${user?.username}/post/${post._id}`}>
             <Flex gap={3} mb={4} py={5}>
                 <Flex flexDirection={"column"} alignItems={"center"}>
                     <Avatar.Root size={"md"} >
                         <Avatar.Fallback name={user?.name} />
-                        <Avatar.Image src={user?.profilePic} />
+                        <Avatar.Image src={user?.profilePic}
+                            onClick={(e) => {
+                                e.preventDefault();
+                                navigate(user.username)
+                            }}
+                        />
                     </Avatar.Root>
                     <Box w={1} h={"full"} bg="gray.400" my={2}></Box>
                     <Box position={"relative"} w={"full"} pt={"2px"}>
-
                         <AvatarGroup gap="0" spaceX="-4" size="xs" pt={"2px"} top={"0px"} left={"15px"} mr={2}>
-                            <Avatar.Root>
-                                <Avatar.Fallback name="Baki Ani" />
-                                <Avatar.Image src="https://cdn.myanimelist.net/r/84x124/images/characters/7/284129.webp?s=a8998bf668767de58b33740886ca571c" />
-                            </Avatar.Root>
+                            {post.replies[0] &&
+                                <Avatar.Root>
+                                    <Avatar.Fallback name={post.replies[0].name} />
+                                    <Avatar.Image src={post.replies[0].userProfilePic} />
+                                </Avatar.Root>
+                            }
+                            {post.replies[1] &&
+                                <Avatar.Root>
+                                    <Avatar.Fallback name={post.replies[1].name} />
+                                    <Avatar.Image src={post.replies[1].userProfilePic} />
+                                </Avatar.Root>
+                            }
+                            {post.replies[2] &&
+                                <Avatar.Root>
+                                    <Avatar.Fallback name={post.replies[2].name} />
+                                    <Avatar.Image src={post.replies[2].userProfilePic} />
+                                </Avatar.Root>
+                            }
 
-                            <Avatar.Root>
-                                <Avatar.Fallback name="Baki Ani" />
-                                <Avatar.Image src="https://cdn.myanimelist.net/r/84x124/images/characters/7/284129.webp?s=a8998bf668767de58b33740886ca571c" />
-                            </Avatar.Root>
-
-                            <Avatar.Root>
-                                <Avatar.Fallback name="Uchiha Chan" />
-                                <Avatar.Image src="https://cdn.myanimelist.net/r/84x124/images/characters/9/105421.webp?s=269ff1b2bb9abe3ac1bc443d3a76e863" />
-                            </Avatar.Root>
                             {/* <Avatar.Root variant="solid">
                                 <Avatar.Fallback>+2</Avatar.Fallback>
                             </Avatar.Root> */}
                         </AvatarGroup>
+
                     </Box>
                 </Flex>
                 <Flex flex={1} flexDirection={"column"} gap={2}>
                     <Flex justifyContent={"space-between"} w={"full"}>
                         <Flex w={"full"} alignItems={"center"}>
-                            <Text fontSize={"sm"} fontWeight={"bold"}>{user?.name}</Text>
+                            <Text
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    navigate(user.username)
+                                }}
+                                fontSize={"sm"} fontWeight={"bold"}>{user?.username}</Text>
                             <Image src="/verified.png" h={4} ml={1}></Image>
                         </Flex>
                         <Flex gap={4} alignItems={"center"}>
-                            <Text fontSize={"sm"} color={"gray.400"}>1d</Text>
-                            <BsThreeDots />
+                            <Text fontSize={"sm"} color={"gray.400"}>{formatDistance( new Date(post.createdAt), new Date() , {addSuffix: true})}</Text>
 
                         </Flex>
                     </Flex>
@@ -106,6 +121,6 @@ export default function Post({ post, postedBy }) {
             </Flex>
 
         </Link>
-        
+
     )
 }
