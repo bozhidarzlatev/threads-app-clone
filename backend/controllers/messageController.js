bc7d5df0aaa349f94b46db0353d4441bc567f7e6
bozhidarzlatev <156793066+bozhidarzlatev@users.noreply.github.com>
2025-09-10 18:04:39 +0300

    added backend routes for messages

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,8 +1,98 @@
+import Message from "../models/messageModel";
+import Conversation from "../models/conversationModel ";
+
 const sentMessage = async (req, res) => {
     console.log(`hi`);
+
+    try {
+        const { recipientId, message } = req.body;
+        const senderId = req.user._id;
+
+        let conversation = await Conversation.findOne({
+            participants: { $all: [senderId, recipientId] }
+        })
+
+        if (!conversation) {
+            conversation = new Conversation({
+                participants: [senderId, recipientId],
+                lastMessage: {
+                    text: message,
+                    sender: senderId
+                }
+            })
+
+            await conversation.save()
+        }
+
+        const newMessage = new Message({
+            conversationId: conversation._id,
+            sender: senderId,
+            text: message
+        })
+
+        await Promise.all([
+            newMessage.save(),
+            conversation.updateOne({
+                lastMessage: {
+                    text: message,
+                    sender: senderId
+                }
+            })
+        ])
+
+        res.status(201).json(newMessage)
+
+    } catch (error) {
+        res.status(500).json({ errod: error.message })
+
+    }
+
+}
+
+const getMessages = async (req, res) => {
+    const { otherUserId } = req.params;
+    const userId = req.user._id
+
+    try {
+        const conversation = await Conversation.findOne({
+            participants: { $all: [userId, otherUserId] }
+        })
+
+        if (!conversation) {
+            return res.status(404).json({ errod: "Conversation not found!" })
+        }
+
+        const messages = await Message.find({
+            conversationId: conversation._id
+        }).sort({ createdAt: 1 })
+
+    } catch (error) {
+        res.status(500).json({ errod: error.message })
+
+    }
+}
+
+const getConversations = async (req, res) => {
+    const userId = req.user._id;
     
+    try {
+
+        const conversations = await Conversation.find({
+            participants: userId
+        }).populate({
+            path: "participants",
+            select: "username profilePic"
+        });
+
+        res.status(200).json(conversations);
+    } catch (error) {
+        res.status(500).json({ errod: error.message })
+
+    }
 }
 
 export const messageController = {
-    sentMessage
+    sentMessage,
+    getMessages,
+    getConversations
 }
--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -6,6 +6,8 @@ import { messageController } from '../controllers/messageController.js';
 const router = express.Router();
 
 
+router.get("/conversations", protectRoute, messageController.getConversations)
+router.get("/:otherUserId", protectRoute, messageController.getMessages)
 router.post("/", protectRoute, messageController.sentMessage)
 
 
